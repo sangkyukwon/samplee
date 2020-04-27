1532c1895dfb13136a01f73246b5602cf1f8fddb
Seongin-Ahn <rodls_su2585@naver.com>
2020-04-27 18:55:44 +0900

    로그인 페이지 수정

---
+++ app/src/main/java/com/example/samsung/sample/loginActivity.java
@@ -0,0 +1,23 @@
+package com.example.samsung.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class loginActivity extends AppCompatActivity {
+    Button loginbtn1, loginbtn2, findbtn, joinbtn;
+    EditText idtext,pwtext;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+    }
+
+}
