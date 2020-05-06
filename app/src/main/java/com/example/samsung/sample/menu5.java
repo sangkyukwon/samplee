5f376b0b1e099f6fd9ebafce8e780fc245f77266
sangkyukwon <59826679+sangkyukwon@users.noreply.github.com>
2020-05-06 20:35:53 +0900

    kko+map(오류수정)

--- app/src/main/java/com/example/samsung/sample/menu5.java
+++ app/src/main/java/com/example/samsung/sample/menu5.java
@@ -1,5 +1,6 @@
 package com.example.samsung.sample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class menu5 extends Fragment {
 
@@ -14,6 +16,19 @@ public class menu5 extends Fragment {
     @Override
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_main5, container, false);
+       View view = inflater.inflate(R.layout.activity_main5, container, false);
+
+        Button login =(Button)view.findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myintent = new Intent(getActivity(),loginActivity.class);
+                startActivity(myintent);
+            }
+
+
+        });
+
+        return view;
     }
 }
