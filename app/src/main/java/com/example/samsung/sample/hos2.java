074cdace80a3f14d1565a59402dd5682435a32af
sangkyukwon <59826679+sangkyukwon@users.noreply.github.com>
2020-05-13 11:48:04 +0900

    map 분류

---
+++ app/src/main/java/com/example/samsung/sample/hos2.java
@@ -0,0 +1,22 @@
+package com.example.samsung.sample;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class hos2 extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hos2, container, false);
+    }
+}
--- app/src/main/java/com/example/samsung/sample/menu3.java
+++ app/src/main/java/com/example/samsung/sample/menu3.java
@@ -4,15 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 
-public class menu3 extends Fragment
-
-{
+public class menu3 extends Fragment {
+    hos fragment1;
+    hos2 fragment2;
 
 
 
@@ -22,6 +25,26 @@ public class menu3 extends Fragment
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_main3, container, false);
+         fragment1 = new hos();
+         fragment2 = new hos2();
+
+        Button button1=(Button)view.findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.map,fragment1);
+
+            }
+        });
+
+        Button button2= (Button)view.findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.map,fragment2);
+            }
+        });
+
 
 
         return view;
