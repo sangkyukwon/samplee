5fc115fe42cfa35d90f9414691d5d473a2c83e0d
sangkyukwon <59826679+sangkyukwon@users.noreply.github.com>
2020-06-03 22:49:16 +0900

    toggle naver

--- app/src/main/java/com/example/samsung/sample/loginActivity.java
+++ app/src/main/java/com/example/samsung/sample/loginActivity.java
@@ -1,6 +1,8 @@
 package com.example.samsung.sample;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
@@ -19,13 +21,23 @@ import com.kakao.usermgmt.callback.LogoutResponseCallback;
 import com.kakao.usermgmt.callback.MeV2ResponseCallback;
 import com.kakao.usermgmt.response.MeV2Response;
 import com.kakao.util.exception.KakaoException;
+import com.nhn.android.naverlogin.OAuthLogin;
+import com.nhn.android.naverlogin.OAuthLoginHandler;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.sql.BatchUpdateException;
+
+import static com.nhn.android.naverlogin.OAuthLogin.mOAuthLoginHandler;
 
 public class loginActivity extends AppCompatActivity {
 
     private SessionCallback sessionCallback;
-
+    private  static OAuthLogin mOAuthLoginModule;
 
 
     @Override
@@ -117,6 +129,105 @@ public class loginActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "로그인 도중 오류가 발생했습니다. 인터넷 연결을 확인해주세요: "+e.toString(), Toast.LENGTH_SHORT).show();
                 }
             }
+
+    private void setNaver() {
+        mOAuthLoginModule = OAuthLogin.getInstance();
+        mOAuthLoginModule.init(this, "wsTpNyGapr8BMpvGC12_", "HmKDlXZw_7", "clientName");
+
+        //mOAuthLoginButton = findViewById(R.id.button_naverlogin);
+
+        binding.buttonNaverlogin.setOAuthLoginHandler(mOAuthLoginHandler);
+        mOAuthLoginModule.startOauthLoginActivity(this, mOAuthLoginHandler);
+    }
+
+    private OAuthLoginHandler mOAuthLoginHandler = new OAuthLoginHandler() {
+        @Override
+        public void run(boolean success) {
+            if (success) {
+                password = mOAuthLoginModule.getAccessToken(act);
+                //getNaverInfo(accessToken);
+                ProfileTask task = new ProfileTask();
+                // 이 클래스가 유저정보를 가져오는 업무를 담당합니다.
+                task.execute(password);
+
+                String refreshToken = mOAuthLoginModule.getRefreshToken(act);
+                long expiresAt = mOAuthLoginModule.getExpiresAt(act);
+                String tokenType = mOAuthLoginModule.getTokenType(act);
+
+            } else {
+                String errorCode = mOAuthLoginModule.getLastErrorCode(act).getCode();
+                String errorDesc = mOAuthLoginModule.getLastErrorDesc(act);
+                Toast.makeText(act, "errorCode:" + errorCode
+                        + ", errorDesc:" + errorDesc, Toast.LENGTH_SHORT).show();
+            }
+        };
+    };
+
+    class ProfileTask extends AsyncTask<String, Void, String> {
+        String result;
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String token = strings[0];// 네이버 로그인 접근 토큰;
+            String header = "Bearer " + token; // Bearer 다음에 공백 추가
+            try {
+                String apiURL = "https://openapi.naver.com/v1/nid/me";
+                URL url = new URL(apiURL);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+                con.setRequestProperty("Authorization", header);
+                int responseCode = con.getResponseCode();
+                BufferedReader br;
+                if (responseCode == 200) { // 정상 호출
+                    br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                } else {  // 에러 발생
+                    br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+                }
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = br.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                result = response.toString();
+                br.close();
+                System.out.println(response.toString());
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            //result 값은 JSONObject 형태로 넘어옵니다.
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                //넘어온 result 값을 JSONObject 로 변환해주고, 값을 가져오면 되는데요.
+                // result 를 Log에 찍어보면 어떻게 가져와야할 지 감이 오실거에요.
+                JSONObject object = new JSONObject(result);
+                Log.d(CommonUtil.TAG,"결과 : "+result);
+                if (object.getString("resultcode").equals("00")) {
+                    JSONObject jsonObject = new JSONObject(object.getString("response"));
+                    email = jsonObject.getString("id");
+                    Log.d("jsonObject", jsonObject.toString());
+
+                /*SharedPreferences.Editor editor = activity.userData.edit();
+                editor.putString("email", jsonObject.getString("email"));
+                editor.putString("name", jsonObject.getString("name"));
+                editor.putString("nickname", jsonObject.getString("nickname"));
+                editor.putString("profile", jsonObject.getString("profile_image"));
+                editor.apply();
+                Intent intent = new Intent(activity, MainActivity.class);
+                activity.startActivity(intent);*/
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
         }
 
 
