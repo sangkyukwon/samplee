81eefab6ee1e8639f373cdfab6572d87962b0d25
sangkyukwon <59826679+sangkyukwon@users.noreply.github.com>
2020-06-03 14:28:33 +0900

    toggle xml/ 기능

--- app/src/main/java/com/example/samsung/sample/Ex.java
+++ app/src/main/java/com/example/samsung/sample/Ex.java
@@ -16,20 +16,16 @@ public class Ex extends AppCompatActivity {
 //입력값 초기 6/1
         String strNickname, strProfile;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-            TextView tvNickname = findViewById(R.id.kkoid);
-            TextView tvProfile = findViewById(R.id.kko);
-
-            Intent intent = getIntent();
-            strNickname = intent.getStringExtra("name");
-            strProfile = intent.getStringExtra("profile");
-
-            tvNickname.setText(strNickname);
-            tvProfile.setText(strProfile);
+//
+//            TextView tvNickname = findViewById(R.id.kkoid);
+//            TextView tvProfile = findViewById(R.id.kko);
+//
+//            Intent intent = getIntent();
+//            strNickname = intent.getStringExtra("name");
+//            strProfile = intent.getStringExtra("profile");
+//
+//            tvNickname.setText(strNickname);
+//            tvProfile.setText(strProfile);
 
 
 
