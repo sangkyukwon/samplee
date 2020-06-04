c58f097dcfc68786646f3f966fd5946e60d49cce
sangkyukwon <59826679+sangkyukwon@users.noreply.github.com>
2020-06-04 15:24:18 +0900

    06/03

---
+++ app/src/main/java/com/example/samsung/sample/Weaderitem.java
@@ -0,0 +1,14 @@
+package com.example.samsung.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Weaderitem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weaderitem);
+    }
+}
