793bb14e7f695c5bccd734fb0414fc76609674c6
sangkyukwon <59826679+sangkyukwon@users.noreply.github.com>
2020-06-20 23:41:43 +0900

    teset

---
+++ app/src/main/java/com/example/samsung/sample/Res_sucess.java
@@ -0,0 +1,14 @@
+package com.example.samsung.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Res_sucess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_res_sucess);
+    }
+}
---
+++ app/src/main/java/com/example/samsung/sample/Reservation_info.java
@@ -0,0 +1,14 @@
+package com.example.samsung.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Reservation_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation_info);
+    }
+}
